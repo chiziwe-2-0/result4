0a2b7c9959af3d97cbaf5b979e4b152eb538b0d4
Nikita Olimpiev <nikitaolimpiev97@gmail.com>
2021-11-03 01:08:00 +0300

    work?

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-require('http').Server((req, res) => {
-    const author = 'itmo307702'
-  
-    res.setHeader('X-Author', author)
-    res.setHeader('Content-Type', 'application/json')
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
-    res.setHeader('Access-Control-Allow-Headers', 'x-test,Content-Type,Accept,Access-Control-Allow-Headers')
-  
-    if (req.url === '/result4/') {
-      let body = [];
-      req.on('data', (chunk) => {
-        body.push(chunk);
-      }).on('end', () => {
-        body = Buffer.concat(body).toString();
-  
-        return res.end(JSON.stringify({
-          message: author,
-          'x-result': req.headers['x-test'],
-          'x-body': body,
-        }))
-      })
-    } else {
-      res.end(author)
+const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,DELETE',
+    'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, x-test',
+};
+
+const s = require('http').Server((req,res) => {
+    let body = '';
+    switch (req.url) {
+        case '/result4/':
+            req.on('data', (chunk) => {
+                body += chunk;
+            });
+            req.on('end', () => {
+                res.writeHead(200, {
+                    'Content-Type': 'application/json; charset=utf-8',
+                    ...CORS
+                });
+                res.write(JSON.stringify({
+                    message: 'itmo307702',
+                    'x-result': req.headers['x-test'],
+                    'x-body': body
+                }));
+                return res.end('\n');
+            });
+            break;
+        default:
+            res.writeHead(200, {
+                ...CORS
+            });
+
+            return res.end('\n');
+            break;
     }
-  }).listen(process.env.PORT)
+    // res.end('\n');
+});
+s.listen(4321);
